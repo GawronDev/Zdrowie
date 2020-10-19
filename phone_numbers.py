24f984e488ca5829c4cef079945d40c5511271f0
FabianOlczakk <fabianolczak22@gmail.com>
2020-10-19 20:38:35 +0200

    problem

--- phone_numbers.py
+++ phone_numbers.py
@@ -6,10 +6,14 @@ from kivy.properties import StringProperty
 class CountrySelect(OneLineAvatarIconListItem):
     def __init__(self, **kwargs):
         super(CountrySelect, self).__init__(**kwargs)
-        self.text = '123'
 
 
 class CountryWidget(IconLeftWidget):
     def __init__(self, **kwargs):
         super(CountryWidget, self).__init__(**kwargs)
-        self.icon = 'images/logo16x16.png'
+        self.icon = 'images/countries/Polska.png'
+
+
+class Show():
+    def __init__(self):
+        self.icon = 'images/countries/Austria.png'
