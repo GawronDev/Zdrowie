7a861eda7d4f310c1755fa38789c7425deaa7937
FabianOlczakk <fabianolczak22@gmail.com>
2020-10-21 15:13:02 +0200

    Stworzyłem listę krajów w numerach alarmowych.

--- phone_numbers.py
+++ phone_numbers.py
@@ -1,17 +1,40 @@
 from kivymd.uix.list import MDList
 from kivy.clock import Clock
-from kivymd.uix.list import OneLineListItem
+from kivymd.uix.list import OneLineIconListItem, IconLeftWidget
+import pickle
 
+
+class CountryOneLineIconListItem(OneLineIconListItem):
+    pass
 
 
 class Show(MDList):
     def __init__(self, **kwargs):
         super(Show, self).__init__(**kwargs)
+        self.numbers = self.pickle_list()[3]
+        self.countries = []
+        for self.key, self.value in self.numbers.items():
+            icon = IconLeftWidget(icon=f'images/countries/{self.key}.png', )
+            self.item = CountryOneLineIconListItem(text=str(self.key))
+            self.add_widget(self.item)
+            self.item.add_widget(icon)
+    
+    @staticmethod
+    def down_button(country):
+        print(country)
 
-        Clock.schedule_once(self.widgets, 1)
-
-    def widgets(self, *args):
-        item = OneLineListItem(text='Austria')
-        self.add_widget(item)
-
-
+    @staticmethod
+    def pickle_list():
+        load = []
+        with open('countries_data.dat', 'rb') as file:
+            a = pickle.load(file)
+            load.append(a)
+            b = pickle.load(file)
+            load.append(b)
+            c = pickle.load(file)
+            load.append(c)
+            d = pickle.load(file)
+            load.append(d)
+            e = pickle.load(file)
+            load.append(e)
+            return load
