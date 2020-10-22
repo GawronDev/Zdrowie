f8851b95bdda157589c2dbadb160f4509fb05a10
FabianOlczakk <fabianolczak22@gmail.com>
2020-10-22 20:38:47 +0200

    Dodałem numery alarmowe.

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
-from kivymd.app import MDApp
-from kivymd.uix.button import MDIconButton
 
-
-class MainApp(MDApp):
-    def build(self):
-        return MDIconButton(icon = 'images/countries/Austria.png')
-
-
-if __name__ == '__main__':
-    MainApp().run()
